84c005c8cd4d15ea9d7e0a0f2292997474bce995
api admin <api.admin@bamboo_agent_mac.kkbdomain.com>
2015-07-21 14:11:55 +0300

    Initial Commit

---
+++ WebserviceExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebserviceExample
+//
+//  Created by api admin on 7/21/15.
+//  Copyright (c) 2015 KKB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebserviceExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebserviceExample
+//
+//  Created by api admin on 7/21/15.
+//  Copyright (c) 2015 KKB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
